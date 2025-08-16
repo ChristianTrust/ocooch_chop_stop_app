b09b6cf30dd24bb88bfc65892301dfea6091f917
ChristianTrust <christiantrustm@gmail.com>
2025-08-16 10:29:11 -0500

    Final changes

--- app/src/main/java/com/christian/ocoochchopstop/ui/elements/distanceDisplay.kt
+++ app/src/main/java/com/christian/ocoochchopstop/ui/elements/distanceDisplay.kt
@@ -5,10 +5,16 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
+import com.christian.ocoochchopstop.ui.util.ocoochCard
 import com.christian.ocoochchopstop.ui.viewmodel.ChopStopViewModel
 import kotlinx.coroutines.delay
 
@@ -35,6 +42,8 @@ fun distanceDisplay(
     val displayDistance: String
     val fontSize: TextUnit
     val unitMarkerColor: Color
+    var expanded by remember { mutableStateOf(false) }
+    val cornerRadius = 24.dp
 
     val backgroundColor = if (chop.isInvalidInput) {
         MaterialTheme.colorScheme.error
@@ -62,46 +71,95 @@ fun distanceDisplay(
         }
     }
 
-    Row(
-        modifier = modifier
-            .width(width)
-            .height(48.dp)
-            .clip(CircleShape)
-            .border(3.dp, borderColor, CircleShape)
-            .background(backgroundColor)
-            .zIndex(100f)
-            .clickable {
-                if (distance == "STATE: Disconnected") {
-                    navController.navigate("dino_game")
-                }
-            },
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
+    Column(
+        modifier = Modifier.width(width)
     ) {
-        Text(
-            text = displayDistance,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onSurface,
-            fontSize = fontSize,
-            fontWeight = FontWeight.Bold
-        )
-
-        val unitHeight = if (chop.isInch) 48.dp else 28.dp
-        val unitAlign = if (chop.isInch) Alignment.CenterVertically else Alignment.Bottom
         Row(
-            modifier = Modifier
-                .height(unitHeight),
-            verticalAlignment = unitAlign,
-            horizontalArrangement = Arrangement.Start
+            modifier = modifier
+                .width(width)
+                .height(48.dp)
+                .clip(CircleShape)
+                .border(3.dp, borderColor, CircleShape)
+                .background(backgroundColor)
+                .zIndex(100f)
+                .clickable {
+                    @Suppress("KotlinConstantConditions") // Testing
+                    if (distance == "STATE: Disconnected" && false) {
+                        navController.navigate("dino_game")
+                    } else {
+                        expanded = true
+                    }
+                },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(
-                text = chop.unitMarker,
-                textAlign = TextAlign.Start,
+                text = displayDistance,
+                textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyLarge,
-                color = unitMarkerColor,
-                fontSize = if (chop.isInch) 32.sp else 16.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = fontSize,
                 fontWeight = FontWeight.Bold
+            )
+
+            val unitHeight = if (chop.isInch) 48.dp else 28.dp
+            val unitAlign = if (chop.isInch) Alignment.CenterVertically else Alignment.Bottom
+            Row(
+                modifier = Modifier
+                    .height(unitHeight),
+                verticalAlignment = unitAlign,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = chop.unitMarker,
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = unitMarkerColor,
+                    fontSize = if (chop.isInch) 32.sp else 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(width)
+                .clip(RoundedCornerShape(cornerRadius))
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .border(4.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(cornerRadius)),
+            shape = RoundedCornerShape(cornerRadius)
+        ) {
+            ocoochCard(
+                text = "Recalibrate",
+                onClick = {
+                    chop.isCalibrating = true
+                    expanded = false
+                },
+                modifier = Modifier
+                    .height(64.dp)
+                    .padding(12.dp, 4.dp),
+                colors = listOf(
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.colorScheme.onPrimary
+                ),
+                fontSize = 16
+            )
+            ocoochCard(
+                text = "Home",
+                onClick = {
+                    chop.home(false)
+                    expanded = false
+                },
+                modifier = Modifier
+                    .height(64.dp)
+                    .padding(12.dp, 4.dp),
+                colors = listOf(
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.colorScheme.onPrimary
+                ),
+                fontSize = 16
             )
         }
     }
--- app/src/main/java/com/christian/ocoochchopstop/ui/elements/ocoochPopupAlert.kt
+++ app/src/main/java/com/christian/ocoochchopstop/ui/elements/ocoochPopupAlert.kt
@@ -27,7 +27,8 @@ fun ocoochPopupAlert(
     message: String,
     onConfirm: (() -> Unit)? = null,
     onCancel: () -> Unit,
-    properties: PopupProperties = PopupProperties(focusable = true, dismissOnBackPress = true)
+    properties: PopupProperties = PopupProperties(focusable = true, dismissOnBackPress = true),
+    content: @Composable () -> Unit = {}
 ) {
     if (show) {
         Popup(
@@ -38,83 +39,82 @@ fun ocoochPopupAlert(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surfaceContainerHighest)
                     .clickable { onCancel() },
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
                 Column(
                     modifier = Modifier
-                        .weight(1f),
-                    verticalArrangement = Arrangement.Center,
+                        .padding(16.dp)
+                        .width(320.dp)
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(MaterialTheme.colorScheme.surfaceContainer),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Column(
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onTertiary,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Box(
                         modifier = Modifier
-                            .padding(16.dp)
-                            .width(320.dp)
-                            .clip(RoundedCornerShape(12.dp))
-                            .background(MaterialTheme.colorScheme.tertiaryContainer),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
                     ) {
-                        Spacer(modifier = Modifier.height(8.dp))
-
-                        Text(
-                            text = title,
-                            style = MaterialTheme.typography.bodyLarge,
-                            color = MaterialTheme.colorScheme.onSurface,
-                            fontWeight = FontWeight.Bold
-                        )
-
-                        Spacer(modifier = Modifier.height(8.dp))
-
                         Text(
                             text = message,
                             style = MaterialTheme.typography.bodyMedium,
                             color = MaterialTheme.colorScheme.onSurface
                         )
+                    }
 
-                        Spacer(modifier = Modifier.height(8.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        ocoochCard(
+                            icon = if (optional) Icons.Filled.Close else Icons.Filled.Check,
+                            onClick = { onCancel() },
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .height(48.dp)
+                                .weight(1f),
+                            fontSize = 32,
+                            colors = listOf(
+                                MaterialTheme.colorScheme.surface,
+                                MaterialTheme.colorScheme.onSurface
+                            )
+                        )
 
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
+                        if (optional) {
+                            Spacer(modifier = Modifier.height(8.dp))
+
                             ocoochCard(
-                                icon = if (optional) Icons.Filled.Close else Icons.Filled.Check,
-                                onClick = { onCancel() },
+                                icon = Icons.Filled.Check,
+                                onClick = { onConfirm?.invoke() },
                                 modifier = Modifier
                                     .padding(8.dp)
                                     .height(48.dp)
                                     .weight(1f),
                                 fontSize = 32,
                                 colors = listOf(
-                                    MaterialTheme.colorScheme.tertiary,
+                                    MaterialTheme.colorScheme.surface,
                                     MaterialTheme.colorScheme.onSurface
                                 )
                             )
-
-                            if (optional) {
-                                Spacer(modifier = Modifier.height(8.dp))
-
-                                ocoochCard(
-                                    icon = Icons.Filled.Check,
-                                    onClick = { onConfirm?.invoke() },
-                                    modifier = Modifier
-                                        .padding(8.dp)
-                                        .height(48.dp)
-                                        .weight(1f),
-                                    fontSize = 32,
-                                    colors = listOf(
-                                        MaterialTheme.colorScheme.tertiary,
-                                        MaterialTheme.colorScheme.onSurface
-                                    )
-                                )
-                            }
                         }
                     }
                 }
-                Spacer(modifier = Modifier.weight(1f))
+
+                content()
             }
         }
     }
